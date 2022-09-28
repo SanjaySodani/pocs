9acf8bb0e4c6a095e98627c2b5672dd2660a3ad0
Sanjay <sanjay.sodani@thesmartq.com>
2022-09-28 19:25:07 +0530

    second commit

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const openWindow = () => {
 const hrefWindow = () => {
   window.location.href = "https://www.google.com";
 };
+
+const forceOpen = () => {
+  let a = document.createElement("a");
+  a.setAttribute("href", "https://www.google.com");
+  a.setAttribute("target", "_blank");
+  a.click();
+};
