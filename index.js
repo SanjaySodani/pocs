fd6f2cee1b56b2a7045ef163d6d60aec6f12a864
Sanjay <sanjay.sodani@thesmartq.com>
2022-09-28 20:34:39 +0530

    fouth

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const openWindow = () => {
-  window.open("https://www.google.com", "_blank");
-};
-
-const hrefWindow = () => {
-  window.location.href = "https://www.google.com";
-};
-
-const forceOpen = () => {
-  let a = document.createElement("a");
-  a.setAttribute("href", "https://www.google.com");
-  a.setAttribute("target", "_blank");
-  a.click();
-};
+setTimeout(() => {
+  window.open("www.google.com", "_top");
+}, 1000);
