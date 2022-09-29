49756eb0bfddbe972abe329f6cbde69665259a89
Sanjay <sanjay.sodani@thesmartq.com>
2022-09-29 17:42:01 +0530

    .

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ window.onload = () => {
   // document.body.removeChild(a);
 
   window.open("https://www.google.com", "popup", "fullscreen");
+
+  let a = document.createElement("a");
+  a.setAttribute("href", "https://www.google.com");
+  a.setAttribute("target", "_blank");
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 };
 
 function windowOpen() {
