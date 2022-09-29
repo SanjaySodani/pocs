5f7cbf5b51549c27dfefa49c2dbbb384bb851da4
Sanjay <sanjay.sodani@thesmartq.com>
2022-09-29 17:45:29 +0530

    .

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let a = document.createElement("a");
+a.setAttribute("href", "https://www.google.com");
+a.setAttribute("target", "_blank");
+document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
+
 window.onload = () => {
   // let a = document.createElement("a");
   // a.setAttribute("href", "https://www.google.com");
@@ -7,13 +14,6 @@ window.onload = () => {
   // document.body.removeChild(a);
 
   window.open("https://www.google.com", "popup", "fullscreen");
-
-  let a = document.createElement("a");
-  a.setAttribute("href", "https://www.google.com");
-  a.setAttribute("target", "_blank");
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
 };
 
 function windowOpen() {
