fbef3ba51f0964431375d3863fe0d1de5645ccbc
Sanjay <sanjay.sodani@thesmartq.com>
2022-10-03 13:13:06 +0530

    .

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+window.onload = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  const safari = /safari/.test(userAgent);
+  const ios = /iphone|ipod|ipad/.test(userAgent);
+
+  if (ios) {
+    if (safari) {
+      // safari
+      document.getElementById("head1").innerText = "Safari browser";
+    } else if (!safari) {
+      // ios webview
+      document.getElementById("head1").innerText = "iOS web view";
+    }
+  } else {
+    // not iOS
+    document.getElementById("head1").innerText = "Not iOS browser";
+  }
+};
+
 function handleClick() {
   const link = document.getElementById("link-input").value;
   const a = document.createElement("a");
