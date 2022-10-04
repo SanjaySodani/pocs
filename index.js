032b9cffdc7382cc7a8efb3e05f8b34e77de5bc2
Sanjay <sanjay.sodani@thesmartq.com>
2022-10-04 13:42:02 +0530

    .

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-window.onload = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  const safari = /safari/.test(userAgent);
-  const ios = /iphone|ipod|ipad/.test(userAgent);
-
-  if (ios) {
-    if (safari) {
-      // safari
-      document.getElementById("head1").innerText = "Safari browser";
-    } else if (!safari) {
-      // ios webview
-      document.getElementById("head1").innerText = "iOS web view";
-    }
-  } else {
-    // not iOS
-    document.getElementById("head1").innerText = "Not iOS browser";
-  }
-
-  document.getElementById("agent").innerText = window.navigator.userAgent;
-
-  document.getElementById("textarea").value = JSON.stringify(
-    window.navigator.standalone,
-    undefined,
-    2
-  );
-};
-
 function handleClick() {
-  const link = document.getElementById("link-input").value;
-  const a = document.createElement("a");
-  a.setAttribute("href", link);
-  a.setAttribute("target", "_blank");
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  const url = document.getElementById("link-input").value;
+  // const a = document.createElement("a");
+  // a.setAttribute("href", link);
+  // a.setAttribute("target", "_blank");
+  // document.body.appendChild(a);
+  // a.click();
+  // document.body.removeChild(a);
+  window.navigator.share({ url });
 }
