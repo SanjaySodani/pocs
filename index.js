084eb0046ef6152191f816cb58f4e1ec3367edd5
Sanjay <sanjay.sodani@thesmartq.com>
2022-10-04 16:35:43 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function handleClick() {
-  const url = document.getElementById("link-input").value;
-  // const a = document.createElement("a");
-  // a.setAttribute("href", link);
-  // a.setAttribute("target", "_blank");
-  // document.body.appendChild(a);
-  // a.click();
-  // document.body.removeChild(a);
-  window.navigator.share({ url });
-}
+window.onload = () => {
+  const a = document.createElement("a");
+  a.setAttribute("href", "someapp://somelink/");
+  a.setAttribute("target", "_blank");
+  a.click();
+};
